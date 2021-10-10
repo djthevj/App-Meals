7e2ca7bbadaf35f4f999477507c26e1b5c6842c2
Deep <djthevj@gmail.com>
2021-10-10 17:54:02 +1100

    development work for various screens

---
+++ flutter_complete_guide/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import './screens/categories_screen.dart';
+import './screens/category_meals_screen.dart';
+import '../screens/meal_details_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          bodyText1: TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1),
+          ),
+          bodyText2: TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1),
+          ),
+          headline6: TextStyle(
+            fontSize: 24,
+            fontFamily: 'RobotoCondensed'
+          )
+        )
+      ),
+      //home: CategoriesScreen(), //root screen
+      routes: {
+        '/': (ctx) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen()
+      },
+    );
+  }
+}
+
---
+++ flutter_complete_guide/lib/screens/categories_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import '../widgets/category_item.dart';
+
+import '../models/dummy_categories.dart';
+
+//First screen
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('DeliMeal'),
+        ),
+        body: GridView(
+          padding: const EdgeInsets.all(15),
+          children: DUMMY_CATEGORIES
+              .map(
+                (categoryData) => CategoryItem(
+                  categoryData.id,
+                  categoryData.title,
+                  categoryData.color,
+                ),
+              )
+              .toList(),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+        ));
+  }
+}
